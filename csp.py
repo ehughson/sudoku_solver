97ae13b2196e6434372b19c02eb1fa93f249e60f
ehughson <ehugh349@gmail.com>
2021-04-21 10:00:38 +0100

    reverted changes back -- only worked on the one puzzle

--- csp.py
+++ csp.py
@@ -1,22 +1,21 @@
-import numpy as np
+import numpy as np 
 from random import sample
 from math import sqrt
 from time import time
 from SudokuPuzzle import SudokuPuzzle
 import queue
 
+#constraint propogation with AC3
+
 class AC3():
     """
     A class to represent a Sudoku solver using Constraint Satisfaction Problem
     Reference : <TO BE ADDED>
-
     About the algorithm
     <TO BE ADDED>
-
     Attributes
     ----------
     csp : object of the SudokuPuzzle class
-
     Methods
     -------
     AC3_solve
@@ -27,47 +26,59 @@ class AC3():
         calculates the heuristic of the current state
     """
 
-    def __init__(self, csp: SudokuPuzzle):
+    def __init__(self, CSP):
         """
         input is the Sudoku puzzle
         """
-        self.grid = csp
+        self.grid = CSP
 
     def AC3_solve(self):
         """
         Solver for AC3 CSP
         """
         q = queue.Queue()
-        for arc in self.grid.constraints:  # add all the arcs to a queue
+        
+        for arc in self.grid.constraints: #add all the arcs to a queue
             q.put(arc)
         while not q.empty():
-            (x, y) = q.get()  # get the first arc (x,y) off the queue
+            (x, y) = q.get() #get the first arc (x,y) off the queue
             values = set(self.grid.values[x])
             for p in values:
                 if not self.isconsistent(p, x, y):
+                    #print("we are removing this value:", p)
+                    #print("before removing value:", self.grid.values[x])
                     self.grid.values[x] = "".join(c for c in self.grid.values[x] if c not in set(p))
+                    #print("after removing values:", self.grid.values[x])
                     if len(self.grid.values[x]) == 0:
+                        #print("here")
                         return False
+
                     for k in (self.grid.units[x]):
                         for v in k:
                             if v != x:
-                                q.put(
-                                    (v, x))  # if the x domain has changed add all arcs of the form (k, x) to the queue
-
+                                q.put((v, x)) #if the x domain has changed add all arcs of the form (k, x) to the queue
+        
+        #print(self.grid.values)
         sudoku_grid = []
         r = 0
         for key in self.grid.values:
             if r == 0:
                 interarray = []
-            r = r + 1
+            r = r +1
             if len(self.grid.values[key]) > 1:
+                #self.display(self.grid.values)
                 print("WE HAVE AN ERROR -- VALUE CONTAINS MORE THAN ONE VALUE")
                 exit(-1)
             interarray.append(self.grid.values[key])
-            if r == self.grid.size:
+            if r == 9:
                 r = 0
                 sudoku_grid.append(interarray)
+            #print(val)
+        #print(sudoku_grid)
+        #self.grid.print_sudoku(self.grid.values)
+        
         return sudoku_grid
+
 
     def isconsistent(self, x, Xi, Xj):
         """
@@ -75,28 +86,49 @@ class AC3():
         """
         for y in self.grid.values[Xj]:
             for peer in self.grid.units[Xi]:
-                if Xj in peer and y != x:
-                    return True
+                if Xj in peer and y!= x:
+                         return True
         return False
 
+  
+
+    
+
 if __name__ == '__main__':
+        base = 3
 
-    #board = [["1",""],["","1"]]
-    # board = [["2","1","",""],["4","","1","2"],["1","","",""],["3","4","","1"]]
-    # board = [["2","","",""],["","","2",""],["","4","",""],["","","","3"]]
-    board = [
-        ["", "", "", "2", "6", "", "7", "", "1"],
-        ["6", "8", "", "", "7", "", "", "9", ""],
-        ["1", "9", "", "", "", "4", "5", "", ""],
-        ["8", "2", "", "1", "", "", "", "4", ""],
-        ["", "", "4", "6", "", "2", "9", "", ""],
-        ["", "5", "", "", "", "3", "", "2", "8"],
-        ["", "", "9", "3", "", "", "", "7", "4"],
-        ["", "4", "", "", "5", "", "", "3", "6"],
-        ["7", "", "3", "", "1", "8", "", "", ""],
-    ]
+        side = base * base
 
-    test_bt = SudokuPuzzle(board)
-    sudoku = AC3(test_bt)
-    final_sudoku = sudoku.AC3_solve()
-    test_bt.print_sudoku(final_sudoku)
+        def pattern(r, c):
+                return (base * (r%base)+r//base+c)%side
+
+        def shuffle(s):
+                return sample(s, len(s))
+
+        rBase = range(base)
+        rows1 = [g*base + r for g in shuffle(rBase) for r in shuffle(rBase)]
+        colum = [g*base +c for g in shuffle(rBase) for c in shuffle(rBase)]
+
+        nums = shuffle(range(1, base*base+1))
+
+        board = [[ nums[pattern(r, c )] for c in colum] for r in rows1]
+        
+        #squares = side*side 
+        #print(3//4)
+        empties = (side*side) * 3//6
+        #empties = 37
+
+        #print(empties)
+
+        for p in sample(range(side*side), empties):
+                board[p//side][p%side] = 0
+        
+        #print(board)
+        print(board)
+        test_bt = SudokuPuzzle(board)
+        #print("the results of sudokupuzzle:",  test_bt.board)
+        #print("the symbols are:", test_bt.symbols)
+        sudoku = AC3(test_bt)
+        final_sudoku = sudoku.AC3_solve()
+        print(final_sudoku)
+        test_bt.print_sudoku(final_sudoku)
