518f4c3439bf8894e6480a0cac8989b4a2ab2123
ehughson <ehugh349@gmail.com>
2021-04-21 14:35:35 +0100

    csp updates

--- csp.py
+++ csp.py
@@ -26,59 +26,47 @@ class AC3():
         calculates the heuristic of the current state
     """
 
-    def __init__(self, CSP):
+    def __init__(self, csp: SudokuPuzzle):
         """
         input is the Sudoku puzzle
         """
-        self.grid = CSP
+        self.grid = csp
 
     def AC3_solve(self):
         """
         Solver for AC3 CSP
         """
         q = queue.Queue()
-        
-        for arc in self.grid.constraints: #add all the arcs to a queue
+        for arc in self.grid.constraints:  # add all the arcs to a queue
             q.put(arc)
         while not q.empty():
-            (x, y) = q.get() #get the first arc (x,y) off the queue
+            (x, y) = q.get()  # get the first arc (x,y) off the queue
             values = set(self.grid.values[x])
             for p in values:
                 if not self.isconsistent(p, x, y):
-                    #print("we are removing this value:", p)
-                    #print("before removing value:", self.grid.values[x])
                     self.grid.values[x] = "".join(c for c in self.grid.values[x] if c not in set(p))
-                    #print("after removing values:", self.grid.values[x])
                     if len(self.grid.values[x]) == 0:
-                        #print("here")
                         return False
-
                     for k in (self.grid.units[x]):
                         for v in k:
                             if v != x:
-                                q.put((v, x)) #if the x domain has changed add all arcs of the form (k, x) to the queue
-        
-        #print(self.grid.values)
+                                q.put(
+                                    (v, x))  # if the x domain has changed add all arcs of the form (k, x) to the queue
+
         sudoku_grid = []
         r = 0
         for key in self.grid.values:
             if r == 0:
                 interarray = []
-            r = r +1
+            r = r + 1
             if len(self.grid.values[key]) > 1:
-                #self.display(self.grid.values)
                 print("WE HAVE AN ERROR -- VALUE CONTAINS MORE THAN ONE VALUE")
                 exit(-1)
             interarray.append(self.grid.values[key])
             if r == self.grid.size:
                 r = 0
                 sudoku_grid.append(interarray)
-            #print(val)
-        #print(sudoku_grid)
-        #self.grid.print_sudoku(self.grid.values)
-        
         return sudoku_grid
-
 
     def isconsistent(self, x, Xi, Xj):
         """
@@ -86,12 +74,9 @@ class AC3():
         """
         for y in self.grid.values[Xj]:
             for peer in self.grid.units[Xi]:
-                if Xj in peer and y!= x:
-                         return True
+                if Xj in peer and y != x:
+                    return True
         return False
-
-  
-
     
 
 if __name__ == '__main__':
@@ -111,7 +96,7 @@ if __name__ == '__main__':
 
         nums = shuffle(range(1, base*base+1))
 
-        board = [[ nums[pattern(r, c )] for c in colum] for r in rows1]
+        board = [[ str(nums[pattern(r, c )]) for c in colum] for r in rows1]
         
         #squares = side*side 
         #print(3//4)
@@ -121,7 +106,7 @@ if __name__ == '__main__':
         #print(empties)
 
         for p in sample(range(side*side), empties):
-                board[p//side][p%side] = 0
+                board[p//side][p%side] = str(0)
         
         #print(board)
         print(board)
